95459b6edfd9c68a25c80aca7fec8358c4fd98d4
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-06-19 10:08:28 -0300

    LOGIN retornando erros OK

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,44 +1,30 @@
 import { loginService, generateToken } from '../services/authService.js'
-import bcrypt from "bcrypt";
+
 
 async function login(req, res){
     try{
-        const { body } = req;
 
-        if(!body){
-            return res.status(400).send({message: 'o corpo da requicição de LOGIN está faltando'})
-        }
-
-        const { email, password, userCategory } = body;
+        const { email, password, userCategory } = req.body;
 
         if(!email || !password || !userCategory){
-            return res.status(400).send({message: 'existem dados faltantes'})
+            return res.status(400).json({message: 'existem dados faltantes'})
         }
 
-        const user = await loginService(email);
+        const { valid, user, message } = await loginService(email, password, userCategory);
+        
+        console.log(valid, user, message)
 
-        if(!user){
-            return res.status(400).send({message: 'usuario ou senha invalidos'})
-        }
-
-        const passwordIsValid = bcrypt.compareSync(password, user.password)
-
-        if(!passwordIsValid){
-            return res.status(400).send({message: 'usuario ou senha invalidos'})
-        }
-
-        const category = userCategory == user.userCategory
-
-        if(!category){
-            return res.status(400).send({message: 'defina a categoria de usuario correta.'})
+        if(!valid){
+            return res.status(400).json({ message })
         }
 
         const token = await generateToken(user.id)
 
-        res.send({token})
+        res.send({token, message})
 
     }catch(err){
         console.log(err)
+        return res.status(500).send({ message: 'erro interno do servidor' })
     }
 }
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js'
 import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
 dotenv.config();
 
@@ -7,11 +8,27 @@ export function generateToken(id){
     return jwt.sign({id: id}, process.env.JWT_SECRET, {expiresIn: 86400})
 }
 
-export function loginService(email){
+export async function loginService(email, password, userCategory){
     try{
-        return User.findOne({email: email}).select(`+password`)
+        
+        const user = await User.findOne({email: email}).select(`+password`)
+        if(!user){
+            return { valid: false, message: 'usuário não encontrado' };
+        }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if(!passwordIsValid){
+            return { valid: false, message: 'senha do usuário incorreta' };
+        }
+
+        if(userCategory !== user.userCategory){
+            return { valid: false, message: 'categoria de usuário incorreta' };
+        }
+
+        return { valid: true, user, message: 'login bem-sucedido'}
     }catch(err){
         console.log(`deu erro no loginService, back `, err)
+        throw new Error('erro no serviço de login');
     }
 }
 
